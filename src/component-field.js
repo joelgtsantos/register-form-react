f4d6d2a32477d482ef686a88a16ca460463b7e4d
Joel Santos <joelgtsantos@gmail.com>
2018-04-11 15:55:40 -0600

    Add semantic, field, form

---
+++ src/component-field.js
@@ -0,0 +1,54 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+class Field extends React.Component {
+  //Using proptypes to validate the props that are passing through the props from component-form
+  static propTypes = {
+    placeholder: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    validate: PropTypes.func,
+    onChange: PropTypes.func.isRequired,
+  };
+
+  state = {
+    value: this.props.value,
+    error: false,
+  };
+
+  //Set clear the field
+  componentWillReceiveProps(update) {
+    this.setState({ value: update.value });
+  }
+
+  onChange = (evt) => {
+    const name = this.props.name;
+    
+    //Get the value
+    const value = evt.target.value;
+
+    //Wheather or not the field has a validate function
+    const error = this.props.validate ? this.props.validate(value) : false;
+
+    //Update state
+    this.setState({ value, error });
+
+    //Propagate up
+    this.props.onChange({ name, value, error });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          placeholder={this.props.placeholder}
+          value={this.state.value}
+          onChange={this.onChange}
+        />
+        <span style={{ color: 'red' }}>{ this.state.error }</span>
+      </div>
+    );
+  }
+};
+
+export default Field;
